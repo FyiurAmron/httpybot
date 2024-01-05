c9d0d367c9973dd948d894783dc28d1900d8f65b
FyiurAmron <spamove@gmail.com>
2024-01-06 00:27:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('https://pardus.at/')
-assert 'Free Browser Game' in browser.title
+options = ChromeOptions()
+options.headless = True
+chrome = webdriver.Chrome(options=options)
+
+chrome.get('https://pardus.at/')
+assert 'Free Browser Game' in chrome.title
