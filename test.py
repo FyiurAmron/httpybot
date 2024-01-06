2b9530e2282d88381c053922ec3c5c9fe35801c5
FyiurAmron <spamove@gmail.com>
2024-01-06 03:41:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -41,7 +41,8 @@ WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'B
 WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Hack Information'))).click()
 
 WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Return to the Black Market')))
-chrome.find_element(By.NAME, 'lookup_name').send_keys('Cras').submit()
+chrome.find_element(By.NAME, 'lookup_name').send_keys('Cras')
+chrome.find_element(By.NAME, 'name_lookup').submit()
 
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Hack"]'))).click()
 
