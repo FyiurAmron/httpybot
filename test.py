f7c5a0b29e5b2561e64f70866f0ac8c7e0c8c6b1
FyiurAmron <spamove@gmail.com>
2024-01-06 20:04:49 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
+import time
+import datetime
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 REQ_APS = 100 # lame hack
 
