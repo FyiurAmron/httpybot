39462f26fd7075186cab15c3b9e212774161aa68
FyiurAmron <spamove@gmail.com>
2024-01-06 01:39:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,17 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.headless = True
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-infobars")
-options.add_argument("--start-maximized")
-options.add_argument("--disable-notifications")
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-infobars')
+options.add_argument('--disable-notifications')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--start-maximized')
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
 
 chrome = webdriver.Chrome(options=options)
 
 chrome.get('https://pardus.at/')
+print(chrome.title)
 assert 'Free Browser Game' in chrome.title
