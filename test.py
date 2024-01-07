2dc23da12a243ee718cd96eec858daf86b94f499
FyiurAmron <spamove@gmail.com>
2024-01-07 17:20:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import datetime
+import random
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,14 +9,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
-REQ_APS = 100 # lame hack
+MIN_APS = 4400
 
 options = ChromeOptions()
 options.headless = True
 for arg in [
   '--headless=new',
   '--use-angle=disabled',
-  '--use-gl=swiftshader',
   '--disable-gpu',
   '--disable-gpu-sandbox',
   '--disable-extensions',
@@ -24,68 +24,96 @@ for arg in [
   '--disable-dev-shm-usage',
   '--start-maximized',
   '--no-sandbox'
-  '--headless=new',
 ]:
   options.add_argument(arg)
 
 chrome = webdriver.Chrome(options=options)
 
+def now():
+  return datetime.datetime.now()
+
+def human_delay():
+  time.sleep(random.randint(1,3))
+
+def switch_to_frame(frame_id):
+  chrome.switch_to.default_content()
+  chrome.switch_to.frame(frame_id)
+
+def log(text):
+  print( f'{now()}: {text}' )  
+
+def wait_until(until_condition):
+  return WebDriverWait(chrome, 10).until(until_condition)
+
+def human_form_fill(name, text):
+  el = wait_until(EC.presence_of_element_located((By.NAME, name)))
+  human_delay()
+  el.send_keys(text)
+  log( f'sent keys "text" to form field "name"' )   
+
+def human_click(by, value, log_msg):
+  el = wait_until(EC.presence_of_element_located((by, value)))
+  human_delay()
+  el.click()
+  log( f'clicked {log_msg}' ) 
+
+def human_link_click(link_text):
+  human_click(By.LINK_TEXT, link_text, f'link "{link_text}"')
+
+def human_selector_click(css_selector):
+  human_click(By.CSS_SELECTOR, css_selector, f'CSS elem "{css_selector}"')
+
+def human_menu_click(target_page):
+  switch_to_frame('menu')
+  human_click(By.CSS_SELECTOR, f'.menubutton > a[href="{target_page}.php"]', f'menu link "{target_page}"')
+  switch_to_frame('main')
+
+def get_text_by_selector(css_selector):
+  return wait_until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector))).text
+
+def get_int_by_selector(css_selector):
+  return int(get_text_by_selector(css_selector))
+
+# note that form.submit() is generally bad idea, and Pardus showcases that very effectively:
+# e.g. direct form submit doesn't work due to JS onClick feeding password's MD5 hash to a hidden input field etc.
+
 chrome.get('https://pardus.at/')
-print(chrome.title)
+log(chrome.title)
 assert 'Free Browser Game' in chrome.title
-chrome.find_element(By.NAME, 'acc').send_keys('spamove@gmail.com')
-chrome.find_element(By.NAME, 'pw').send_keys('qzwxec')
-# form submit doesn't work due to JS magic feeding password's MD5 hash to a hidden input field
-chrome.find_element(By.CLASS_NAME, 'loginbutton').click()
 
-WebDriverWait(chrome, 10).until(EC.title_is('Pardus'))
+human_form_fill('acc', 'spamove@gmail.com')
+human_form_fill('pw', 'p4rdu5')
+human_selector_click('.loginbutton')
+
+wait_until(EC.title_is('Pardus'))
 assert chrome.current_url == 'https://orion.pardus.at/game.php'
-now = datetime.datetime.now()
-print(now)
+log( 'login successful' )
 
-chrome.switch_to.default_content()
-chrome.switch_to.frame('main')
+switch_to_frame('main')
 
-apsleftEl = chrome.find_element(By.ID, 'apsleft')
-apsleft = int(apsleftEl.text)
-print( "APs left: (before)", apsleft )
-if ( apsleft < REQ_APS ):
-  print( "less than required", REQ_APS, "APs, terminating..." )
+apsleft = get_int_by_selector('#apsleft')
+log( f'APs left: {apsleft}' )
+if ( apsleft < MIN_APS ):
+  log( f'less than minimum {MIN_APS} APs, doing nothing and exiting...' )
   chrome.quit()
   sys.exit()
 
-chrome.find_element(By.LINK_TEXT, 'Land').click()
+human_link_click('Land')
+human_link_click('Black Market')
+human_link_click('Hack Information')
 
-WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Black Market'))).click()
+human_form_fill('lookup_name', 'a')
+human_selector_click('[name="name_lookup"]')
+human_selector_click('#doHackButton')
 
-WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Hack Information'))).click()
+switch_to_frame('msgframe')
 
-WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Return to the Black Market')))
-chrome.find_element(By.NAME, 'lookup_name').send_keys('Cras')
-chrome.find_element(By.NAME, 'name_lookup').submit()
+message = get_text_by_selector('body > table > tbody > tr > td[align="center"] > table > tbody > tr > td:nth-child(2) > font')
 
-WebDriverWait(chrome, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Hack"]'))).click()
+log(message)
 
-chrome.switch_to.default_content()
-chrome.switch_to.frame('msgframe')
+menu_click('main')
 
-messageBox = WebDriverWait(chrome, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'body > table > tbody > tr > td[align="center"] > table > tbody > tr > td:nth-child(2) > font')))
-
-print(messageBox.text)
-
-now = datetime.datetime.now()
-print(now)
-
-chrome.switch_to.default_content()
-chrome.switch_to.frame('menu')
-
-chrome.find_element(By.CSS_SELECTOR, 'a[href="main.php"]').click()
-
-chrome.switch_to.default_content()
-chrome.switch_to.frame('main')
-
-apsleftEl = WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.ID, 'apsleft')))
-apsleft = int(apsleftEl.text)
-print( "APs left: (after)", apsleft )
+log( f'APs left: {get_int_by_selector('#apsleft')}' )
 
 chrome.quit() # not really needed, but added for clarity
